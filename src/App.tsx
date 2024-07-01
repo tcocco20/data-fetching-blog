38bc98fe1566a5b7f84e6e0bc563317f8dbf51af
tcocco20 <theo@coccoscoding.dev>
2024-07-01 10:26:17 -0700

    added basic data fetching, adding styling now

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import { Button } from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
 import { get } from "./util/http";
-import { useEffect, useState } from "react";
-import { type BlogPost } from "./components/BlogPosts";
+import { type ReactNode, useEffect, useState } from "react";
+import BlogPosts, { type BlogPost } from "./components/BlogPosts";
+import fetchingImage from "./assets/data-fetching.png";
+
+type RawDataBlogPost = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
 
   useEffect(() => {
     const fetchPosts = async () => {
-      await get("https://jsonplaceholder.typicode.com/posts");
+      const data = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map((post) => {
+        return {
+          id: post.id,
+          title: post.title,
+          text: post.body,
+        };
+      });
+
+      setFetchedPosts(blogPosts);
     };
+
+    fetchPosts();
   }, []);
 
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
   return (
-    <>
-      <h1 className="text-3xl">React Typescript</h1>
-      <Button>Click me</Button>
-    </>
+    <main className="w-full h-full bg-zinc-900">
+      <Image
+        className="h-32 w-32 object-cover border-2 border-white rounded-full my-8 mx-auto"
+        src={fetchingImage}
+        alt="An abstract image depicting data fetching"
+      />
+      {content}
+    </main>
   );
 }
 
