85ba72473ed02944cdffd636a77887cc4527765a
tcocco20 <theo@coccoscoding.dev>
2024-07-05 15:10:46 -0700

    finished app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { get } from "./util/http";
 import { type ReactNode, useEffect, useState } from "react";
 import BlogPosts, { type BlogPost } from "./components/BlogPosts";
 import fetchingImage from "./assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage";
 
 type RawDataBlogPost = {
   userId: number;
@@ -14,30 +15,39 @@ type RawDataBlogPost = {
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
   const [isFetching, setIsFetching] = useState<boolean>(true);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     const fetchPosts = async () => {
       setIsFetching(true);
-      const data = (await get(
-        "https://jsonplaceholder.typicode.com/posts"
-      )) as RawDataBlogPost[];
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts"
+        )) as RawDataBlogPost[];
 
-      const blogPosts: BlogPost[] = data.map((post) => {
-        return {
-          id: post.id,
-          title: post.title,
-          text: post.body,
-        };
-      });
+        const blogPosts: BlogPost[] = data.map((post) => {
+          return {
+            id: post.id,
+            title: post.title,
+            text: post.body,
+          };
+        });
+        setFetchedPosts(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) setError(error.message);
+      }
 
       setIsFetching(false);
-      setFetchedPosts(blogPosts);
     };
 
     fetchPosts();
   }, []);
 
   let content: ReactNode;
+
+  if (error) {
+    content = <ErrorMessage text={error} />;
+  }
 
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />;
